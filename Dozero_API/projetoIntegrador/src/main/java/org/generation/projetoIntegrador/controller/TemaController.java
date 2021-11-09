0141900ee1086d29ce463784255bd2d8ae1db82a
Felipepereiralima <felipeplima@live.com>
2021-11-09 11:55:09 -0300

    Correção de erro no Controller

--- Dozero_API/projetoIntegrador/src/main/java/org/generation/projetoIntegrador/controller/TemaController.java
+++ Dozero_API/projetoIntegrador/src/main/java/org/generation/projetoIntegrador/controller/TemaController.java
@@ -30,32 +30,33 @@ public class TemaController {
 		return ResponseEntity.ok(repository.findAll());
 	}
 
-	@GetMapping("/{id_tema}") // Achando os temas pelo id_tema
-	public ResponseEntity<Tema> getById(@PathVariable long id_tema) {
-		return repository.findById(id_tema).map(resp -> ResponseEntity.ok(resp))
+	@GetMapping("/{id}") // Achando os temas pelo id_tema
+	public ResponseEntity<Tema> getById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
 	}
 
-	@GetMapping("/nome_categoria/{nome_categoria}") // Achando os temas pelo nome_categoria
-	public ResponseEntity<List<Tema>> getByName(@PathVariable String nome_categoria) {
-		return ResponseEntity.ok(repository.findAllByTemaNomeCategoriaContainingIgnoreCase(nome_categoria));
-	}// apenas para o adm criar
+	@GetMapping("/nome/{nome}") // Achando os temas pelo nome_categoria
+	public ResponseEntity<List<Tema>> getByName(@PathVariable String nome) {
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	} // apenas para o adm criar
 
-	// @GetMapping("/postagens_salvas/{postagens_salvas}")Como fazer??? não sei.
+	//@GetMapping("/postagens_salvas/{postagens_salvas}")Como fazer??? não sei.
 
-	@PostMapping // Criação de um novo dado para a entidade tema
+	@PostMapping //Criação de um novo dado para a entidade tema
 	public ResponseEntity<Tema> postTema(@RequestBody Tema tema) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 	}
 
-	@PutMapping // atualizano dado da entidade tema
+	@PutMapping // atualizando dado da entidade tema
 	public ResponseEntity<Tema> putTema(@RequestBody Tema tema) {
-		return ResponseEntity.ok(repository.save(tema));
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
 	}
 
 	@DeleteMapping("/{id_tema}")
-	public void delete(@PathVariable long id_tema) {
-		repository.deleteById(id_tema);
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
-
 }
+
+
